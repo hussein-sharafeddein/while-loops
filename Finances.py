f8130906eef28ad08e58df714d74808d20537e99
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-21 00:57:22 +0200

    Get ==> inputs

---
+++ Finances.py
@@ -0,0 +1,10 @@
+finances = []
+
+# Get Nabiha's input
+month = input("Enter the month: ")
+while month != "stop":
+  
+  income = float(input("Enter your income: "))
+  rent_pct = float(input("Enter the rent percentage: "))
+  electricity_pct = float(input("Enter the electricity percentage: "))
+  savings_pct = float(input("Enter the savings percentage: "))
